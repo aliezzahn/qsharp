8057a0bbde17891597e6a79248640ee9f80ca72f
cesarzc <cesarzc@microsoft.com>
2024-03-14 00:32:46 -0700

    Implement per-statement capabilities check.

--- compiler/qsc_passes/src/capabilitiesck.rs
+++ compiler/qsc_passes/src/capabilitiesck.rs
@@ -6,25 +6,131 @@ mod tests;
 
 use miette::Diagnostic;
 use qsc_data_structures::span::Span;
-use qsc_fir::fir::{PackageId, PackageStore};
-use qsc_rca::PackageStoreComputeProperties;
+use qsc_fir::{
+    fir::{Block, BlockId, Expr, ExprId, Package, PackageLookup, Pat, PatId, Stmt, StmtId},
+    visit::Visitor,
+};
+use qsc_frontend::compile::RuntimeCapabilityFlags;
+use qsc_rca::{ComputeKind, PackageComputeProperties, RuntimeFeatureFlags};
 use thiserror::Error;
 
 #[derive(Clone, Debug, Diagnostic, Error)]
 pub enum Error {
-    #[error("cannot branch based on a dynamic condition")]
+    #[error("cannot use a dynamic boolean")]
     #[diagnostic(help(
-        "branching based on a dynamic condition, a condition that depends on a measurement result, is not supported by the target"
+        "using a dynamic boolean, a boolean that depends on a measurement result, is not supported by the target"
     ))]
-    #[diagnostic(code("Qsc.CapabilitiesCk.BranchingOnDynamicCondition"))]
-    BranchingOnDynamicCondition(#[label] Span),
+    #[diagnostic(code("Qsc.CapabilitiesCk.UseOfDynamicBool"))]
+    UseOfDynamicBool(#[label] Span),
+
+    #[error("cannot use a dynamic integer")]
+    #[diagnostic(help(
+        "using a dynamic integer, an integer that depends on a measurement result, is not supported by the target"
+    ))]
+    #[diagnostic(code("Qsc.CapabilitiesCk.UseOfDynamicInt"))]
+    UseOfDynamicInt(#[label] Span),
+
+    #[error("cannot use a dynamic double")]
+    #[diagnostic(help(
+        "using a dynamic double, a double that depends on a measurement result, is not supported by the target"
+    ))]
+    #[diagnostic(code("Qsc.CapabilitiesCk.UseOfDynamicDouble"))]
+    UseOfDynamicDouble(#[label] Span),
+
+    #[error("cannot use a dynamically-sized array")]
+    #[diagnostic(help(
+        "using a dynamically-sized array, an array whose size depends on a measurement result, is not supported by the target"
+    ))]
+    #[diagnostic(code("Qsc.CapabilitiesCk.UseOfDynamicallySizedArray"))]
+    UseOfDynamicallySizedArray(#[label] Span),
 }
 
 #[must_use]
 pub fn check_supported_capabilities(
-    _package_id: PackageId,
-    _package_store: &PackageStore,
-    _compute_properties: &PackageStoreComputeProperties,
+    package: &Package,
+    compute_properties: &PackageComputeProperties,
+    capabilities: RuntimeCapabilityFlags,
 ) -> Vec<Error> {
-    Vec::new()
+    let checker = Checker {
+        package,
+        compute_properties,
+        target_capabilities: capabilities,
+        errors: Vec::new(),
+    };
+
+    checker.run()
+}
+
+struct Checker<'a> {
+    package: &'a Package,
+    compute_properties: &'a PackageComputeProperties,
+    target_capabilities: RuntimeCapabilityFlags,
+    errors: Vec<Error>,
+}
+
+impl<'a> Visitor<'a> for Checker<'a> {
+    fn get_block(&self, id: BlockId) -> &'a Block {
+        self.package.get_block(id)
+    }
+
+    fn get_expr(&self, id: ExprId) -> &'a Expr {
+        self.package.get_expr(id)
+    }
+
+    fn get_pat(&self, id: PatId) -> &'a Pat {
+        self.package.get_pat(id)
+    }
+
+    fn get_stmt(&self, id: StmtId) -> &'a Stmt {
+        self.package.get_stmt(id)
+    }
+
+    fn visit_stmt(&mut self, stmt_id: StmtId) {
+        let compute_kind = self
+            .compute_properties
+            .stmts
+            .get(stmt_id)
+            .expect("statement should exist in package compute properties")
+            .inherent;
+
+        let ComputeKind::Quantum(quantum_properties) = compute_kind else {
+            return;
+        };
+
+        let runtime_capabilities = quantum_properties.runtime_features.runtime_capabilities();
+        let missing_capabilities = !self.target_capabilities & runtime_capabilities;
+        let missing_features = quantum_properties
+            .runtime_features
+            .contributing_features(missing_capabilities);
+        let stmt = self.get_stmt(stmt_id);
+        let mut stmt_errors = generate_errors_from_runtime_features(missing_features, stmt.span);
+        self.errors.append(&mut stmt_errors);
+    }
+}
+
+impl<'a> Checker<'a> {
+    fn run(mut self) -> Vec<Error> {
+        self.visit_package(self.package);
+        self.errors
+    }
+}
+
+fn generate_errors_from_runtime_features(
+    runtime_features: RuntimeFeatureFlags,
+    span: Span,
+) -> Vec<Error> {
+    let mut errors = Vec::<Error>::new();
+    if runtime_features.contains(RuntimeFeatureFlags::UseOfDynamicBool) {
+        errors.push(Error::UseOfDynamicBool(span));
+    }
+    if runtime_features.contains(RuntimeFeatureFlags::UseOfDynamicInt) {
+        errors.push(Error::UseOfDynamicInt(span));
+    }
+    if runtime_features.contains(RuntimeFeatureFlags::UseOfDynamicDouble) {
+        errors.push(Error::UseOfDynamicDouble(span));
+    }
+    if runtime_features.contains(RuntimeFeatureFlags::UseOfDynamicallySizedArray) {
+        errors.push(Error::UseOfDynamicallySizedArray(span));
+    }
+    errors
 }
--- compiler/qsc_passes/src/capabilitiesck/tests.rs
+++ compiler/qsc_passes/src/capabilitiesck/tests.rs
@@ -1,18 +1,17 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+#![allow(clippy::needless_raw_string_hashes)]
+
 use expect_test::{expect, Expect};
 
 use crate::capabilitiesck::check_supported_capabilities;
-use indoc::indoc;
 use qsc::{incremental::Compiler, PackageType};
 use qsc_data_structures::language_features::LanguageFeatures;
 use qsc_eval::{debug::map_hir_package_to_fir, lower::Lowerer};
-use qsc_fir::fir::{PackageId, PackageStore};
-use qsc_frontend::compile::{
-    PackageStore as HirPackageStore, RuntimeCapabilityFlags, SourceContents, SourceMap, SourceName,
-};
-use qsc_rca::{Analyzer, PackageStoreComputeProperties};
+use qsc_fir::fir::{Package, PackageId, PackageStore};
+use qsc_frontend::compile::{PackageStore as HirPackageStore, RuntimeCapabilityFlags, SourceMap};
+use qsc_rca::{Analyzer, PackageComputeProperties, PackageStoreComputeProperties};
 
 struct CompilationContext {
     fir_store: PackageStore,
@@ -21,16 +20,20 @@ struct CompilationContext {
 }
 
 impl CompilationContext {
-    pub fn new(sources: impl IntoIterator<Item = (SourceName, SourceContents)>) -> Self {
-        let compiler = Compiler::new(
+    fn new(source: &str) -> Self {
+        let mut compiler = Compiler::new(
             true,
-            SourceMap::new(sources, None),
+            SourceMap::default(),
             PackageType::Lib,
             RuntimeCapabilityFlags::all(),
             LanguageFeatures::default(),
         )
         .expect("should be able to create a new compiler");
         let package_id = map_hir_package_to_fir(compiler.package_id());
+        let increment = compiler
+            .compile_fragments_fail_fast("test", source)
+            .expect("code should compile");
+        compiler.update(increment);
         let mut lowerer = Lowerer::new();
         let fir_store = lower_hir_package_store(&mut lowerer, compiler.package_store());
         let analyzer = Analyzer::init(&fir_store);
@@ -40,6 +43,13 @@ impl CompilationContext {
             compute_properties,
             package_id,
         }
+    }
+
+    fn get_package_compute_properties_tuple(&self) -> (&Package, &PackageComputeProperties) {
+        (
+            self.fir_store.get(self.package_id),
+            self.compute_properties.get(self.package_id),
+        )
     }
 }
 
@@ -57,32 +67,59 @@ fn lower_hir_package_store(
     fir_store
 }
 
-fn check(sources: impl IntoIterator<Item = (SourceName, SourceContents)>, expect: &Expect) {
-    let compilation_context = CompilationContext::new(sources);
-    let errors = check_supported_capabilities(
-        compilation_context.package_id,
-        &compilation_context.fir_store,
-        &compilation_context.compute_properties,
-    );
+fn check(source: &str, expect: &Expect) {
+    let compilation_context = CompilationContext::new(source);
+    let (package, compute_properties) = compilation_context.get_package_compute_properties_tuple();
+    let capabilities =
+        RuntimeCapabilityFlags::ForwardBranching | RuntimeCapabilityFlags::IntegerComputations;
+    let errors = check_supported_capabilities(package, compute_properties, capabilities);
     expect.assert_debug_eq(&errors);
 }
 
 #[test]
-fn simple_program_is_valid() {
-    let sources: [(SourceName, SourceContents); 1] = [(
-        "test".into(),
-        indoc! {"
-            namespace Foo {
-                function A() : Unit {}
-            }
-        "}
-        .into(),
-    )];
-
+fn use_of_dynamic_double_yields_error() {
     check(
-        sources,
+        r#"
+        namespace Foo {
+            operation Bar() : Unit {
+                use q = Qubit();
+                let r = M(q);
+                let d = r == Zero ? 0.0 | 1.0;
+            }
+        }"#,
         &expect![[r#"
-            []
+            [
+                UseOfDynamicDouble(
+                    Span {
+                        lo: 141,
+                        hi: 171,
+                    },
+                ),
+            ]
+        "#]],
+    );
+}
+
+#[test]
+fn use_of_dynamically_sized_array_yields_error() {
+    check(
+        r#"
+        namespace Foo {
+            operation Bar() : Unit {
+                use q = Qubit();
+                let s = M(q) == Zero ? 1 | 2;
+                let a = [1, size = s];
+            }
+        }"#,
+        &expect![[r#"
+            [
+                UseOfDynamicallySizedArray(
+                    Span {
+                        lo: 157,
+                        hi: 179,
+                    },
+                ),
+            ]
         "#]],
     );
 }
