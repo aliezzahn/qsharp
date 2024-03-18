ca88b38582dc3561bb265732ef572b71e735b2aa
cesarzc <cesarzc@microsoft.com>
2024-03-17 22:36:17 -0700

    Detect spec-level runtime features.

--- compiler/qsc_passes/src/capabilitiesck.rs
+++ compiler/qsc_passes/src/capabilitiesck.rs
@@ -16,11 +16,15 @@ pub mod common;
 use miette::Diagnostic;
 use qsc_data_structures::span::Span;
 use qsc_fir::{
-    fir::{Block, BlockId, Expr, ExprId, Package, PackageLookup, Pat, PatId, Stmt, StmtId},
+    fir::{
+        Block, BlockId, CallableImpl, Expr, ExprId, Global, Item, ItemKind, LocalItemId, Package,
+        PackageLookup, Pat, PatId, SpecDecl, SpecImpl, Stmt, StmtId,
+    },
+    ty::FunctorSetValue,
     visit::Visitor,
 };
 use qsc_frontend::compile::RuntimeCapabilityFlags;
-use qsc_rca::{ComputeKind, PackageComputeProperties, RuntimeFeatureFlags};
+use qsc_rca::{ComputeKind, ItemComputeProperties, PackageComputeProperties, RuntimeFeatureFlags};
 use thiserror::Error;
 
 #[derive(Clone, Debug, Diagnostic, Error)]
@@ -90,6 +94,7 @@ pub fn check_supported_capabilities(
         package,
         compute_properties,
         target_capabilities: capabilities,
+        current_callable: None,
         errors: Vec::new(),
     };
 
@@ -100,6 +105,7 @@ struct Checker<'a> {
     package: &'a Package,
     compute_properties: &'a PackageComputeProperties,
     target_capabilities: RuntimeCapabilityFlags,
+    current_callable: Option<LocalItemId>,
     errors: Vec<Error>,
 }
 
@@ -120,17 +126,48 @@ impl<'a> Visitor<'a> for Checker<'a> {
         self.package.get_stmt(id)
     }
 
+    fn visit_callable_impl(&mut self, callable_impl: &'a CallableImpl) {
+        match callable_impl {
+            CallableImpl::Intrinsic => self.check_spec_decl(FunctorSetValue::Empty, None),
+            CallableImpl::Spec(spec_impl) => {
+                self.check_spec_decl(FunctorSetValue::Empty, Some(&spec_impl.body));
+                spec_impl.adj.iter().for_each(|spec_decl| {
+                    self.check_spec_decl(FunctorSetValue::Adj, Some(spec_decl));
+                });
+                spec_impl.ctl.iter().for_each(|spec_decl| {
+                    self.check_spec_decl(FunctorSetValue::Ctl, Some(spec_decl));
+                });
+                spec_impl.ctl_adj.iter().for_each(|spec_decl| {
+                    self.check_spec_decl(FunctorSetValue::CtlAdj, Some(spec_decl));
+                });
+            }
+        };
+    }
+
+    fn visit_item(&mut self, item: &'a Item) {
+        // We only care about callables.
+        if let ItemKind::Callable(callable_decl) = &item.kind {
+            self.set_current_callable(item.id);
+            self.visit_callable_decl(callable_decl);
+            let callable_id = self.clear_current_callable();
+            assert!(item.id == callable_id);
+        }
+    }
+
+    fn visit_spec_impl(&mut self, _: &'a SpecImpl) {
+        panic!("visiting a specialization implementation directly should not happen");
+    }
+
     fn visit_stmt(&mut self, stmt_id: StmtId) {
         let compute_kind = self.compute_properties.get_stmt(stmt_id).inherent;
         let ComputeKind::Quantum(quantum_properties) = compute_kind else {
             return;
         };
 
-        let runtime_capabilities = quantum_properties.runtime_features.runtime_capabilities();
-        let missing_capabilities = !self.target_capabilities & runtime_capabilities;
-        let missing_features = quantum_properties
-            .runtime_features
-            .contributing_features(missing_capabilities);
+        let missing_features = get_missing_runtime_features(
+            quantum_properties.runtime_features,
+            self.target_capabilities,
+        );
         let stmt = self.get_stmt(stmt_id);
         let mut stmt_errors = generate_errors_from_runtime_features(missing_features, stmt.span);
         self.errors.append(&mut stmt_errors);
@@ -138,9 +175,86 @@ impl<'a> Visitor<'a> for Checker<'a> {
 }
 
 impl<'a> Checker<'a> {
+    fn check_spec_decl(
+        &mut self,
+        functor_set_value: FunctorSetValue,
+        spec_decl: Option<&'a SpecDecl>,
+    ) {
+        let current_callable_id = self.get_current_callable();
+        let ItemComputeProperties::Callable(callable_compute_properties) =
+            self.compute_properties.get_item(current_callable_id)
+        else {
+            panic!("expected callable variant of item compute properties");
+        };
+
+        let spec_compute_properties = match functor_set_value {
+            FunctorSetValue::Empty => &callable_compute_properties.body,
+            FunctorSetValue::Adj => callable_compute_properties
+                .adj
+                .as_ref()
+                .expect("adj specialization is none"),
+            FunctorSetValue::Ctl => callable_compute_properties
+                .ctl
+                .as_ref()
+                .expect("ctl specialization is none"),
+            FunctorSetValue::CtlAdj => callable_compute_properties
+                .ctl_adj
+                .as_ref()
+                .expect("ctl_adj specialization is none"),
+        };
+
+        if let ComputeKind::Quantum(quantum_properties) = spec_compute_properties.inherent {
+            let missing_features = get_missing_runtime_features(
+                quantum_properties.runtime_features,
+                self.target_capabilities,
+            );
+            let missing_spec_level_runtime_features =
+                get_spec_level_runtime_features(missing_features);
+
+            // If there are any missing specialization-level runtime features, runtime features that affect the whole
+            // specialization, just generate errors for the missing specialization-level runtime features and do not
+            // generate statement-level or expression-level errors for these specializations.
+            if !missing_spec_level_runtime_features.is_empty() {
+                let current_callable = self
+                    .package
+                    .get_global(current_callable_id)
+                    .expect("callable not present in package");
+                let Global::Callable(callable_decl) = current_callable else {
+                    panic!("");
+                };
+                let mut spec_level_errors = generate_errors_from_runtime_features(
+                    missing_spec_level_runtime_features,
+                    callable_decl.span,
+                );
+                self.errors.append(&mut spec_level_errors);
+                return;
+            }
+        }
+
+        // Visit the specialization block.
+        if let Some(spec_decl) = spec_decl {
+            self.visit_block(spec_decl.block);
+        }
+    }
+
+    fn clear_current_callable(&mut self) -> LocalItemId {
+        self.current_callable
+            .take()
+            .expect("current callable is not set")
+    }
+
+    fn get_current_callable(&self) -> LocalItemId {
+        self.current_callable.expect("current callable is not set")
+    }
+
     fn run(mut self) -> Vec<Error> {
         self.visit_package(self.package);
         self.errors
+    }
+
+    fn set_current_callable(&mut self, id: LocalItemId) {
+        assert!(self.current_callable.is_none());
+        self.current_callable = Some(id);
     }
 }
 
@@ -174,4 +288,18 @@ fn generate_errors_from_runtime_features(
         errors.push(Error::CallToCyclicOperation(span));
     }
     errors
+}
+
+fn get_missing_runtime_features(
+    runtime_features: RuntimeFeatureFlags,
+    target_capabilities: RuntimeCapabilityFlags,
+) -> RuntimeFeatureFlags {
+    let missing_capabilities = !target_capabilities & runtime_features.runtime_capabilities();
+    runtime_features.contributing_features(missing_capabilities)
+}
+
+fn get_spec_level_runtime_features(runtime_features: RuntimeFeatureFlags) -> RuntimeFeatureFlags {
+    const SPEC_LEVEL_RUNTIME_FEATURES: RuntimeFeatureFlags =
+        RuntimeFeatureFlags::CallToCyclicOperation;
+    runtime_features & SPEC_LEVEL_RUNTIME_FEATURES
 }
